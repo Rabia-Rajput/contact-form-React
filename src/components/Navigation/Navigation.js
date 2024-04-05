2ea1c6b566d5856e7347afe4c983ca536713b0ca
Rabia-Rajput <rajput.rabia@outlook.com>
2024-04-05 22:37:47 +0200

    navigation.js file added

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,19 @@
+   //rafce
+import styles from './Navigation.module.css';
+const Navigation = () => {
+  return (
+    <nav className={`${styles.navigation} navbar`}>
+        <div >
+            <img src='/images/logo.png' alt='logo'/>
+        </div>
+        <ul >
+          <li href='#' > Home </li>
+          <li href='#' > About </li>
+          <li href='#' > Contact </li>
+        </ul>
+
+    </nav>
+  );
+};
+
+export default Navigation;
