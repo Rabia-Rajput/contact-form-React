75484dbe4694f453fc470ab5166676719a8f1457
Rabia-Rajput <rajput.rabia@outlook.com>
2024-04-06 01:00:08 +0200

    updated  file

--- src/components/ContactHeader/ContactForm/ContactForm.js
+++ src/components/ContactHeader/ContactForm/ContactForm.js
@@ -1,27 +1,59 @@
 import Button from '../../Button/Button';
-import styles from './Contact.module.css';
-import {MdMessage} from 'react-icons/md';
+import styles from "./Contact.module.css";
+import { MdMessage } from "react-icons/md";
+import { FaPhoneAlt } from "react-icons/fa";
+import { HiMail } from "react-icons/hi";
 
 
 const ContactForm = () => {
   return (
     <section className={styles.container}>
-        <div >
+        <div className={styles.contact_btn}> 
+        <div className={styles.btn} >
            
-           < Button text='VIA SUPPORT CHAT' icon={MdMessage}
+           < Button text='VIA SUPPORT CHAT' icon={<MdMessage fontSize={24}/>}
            />
-           < Button text='VIA CALL' icon={MdMessage}
+           < Button text='VIA CALL' icon={<FaPhoneAlt fontSize={24}/>}
            />
-           < Button text='VIA EMAIL FORM' icon={MdMessage}
-           />
-
-
-
+           
         </div>
+        <div> 
+           < Button isOutline text='VIA EMAIL FORM' icon={<HiMail fontSize={24}/> }
+           />
+
+</div>
+   <form>
+    <div className={styles.contact_form}>
+        <label>Name</label>
+        <input type='text' name='name'/>
+    </div>
+    <div className={styles.contact_form}>
+        <label>E-mail</label>
+        <input type='email' name='email'/>
+    </div>
+    <div className={styles.contact_form}>
+        <label>Text</label>
+        <textarea name='text'rows={8} />
+    </div>
+   <div style={
+    {display:'flex',
+
+justifyContent:'end',}
+   }>
+   < Button  text='SUBMIT' 
+           />
+   </div>
+
+
+   </form>
+   </div>
+
         <div className={styles.img}>
+            <img src='/images/service.png.png'/>
 
 
 
+        
         </div>
     </section>
   )
