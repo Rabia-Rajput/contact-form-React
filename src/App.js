787803283f791406343b289ca3f656934daa3ea5
Rabia-Rajput <rajput.rabia@outlook.com>
2024-04-05 22:36:41 +0200

    App.js file added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import ContactForm from './components/ContactHeader/ContactForm/ContactForm';
+import ContactHeader from './components/ContactHeader/ContactHeader';
+import Navigation from './components/Navigation/Navigation';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>   
+    <Navigation/>
+    <ContactHeader/>
+    <ContactForm />
     </div>
   );
-}
+};
 
 export default App;
